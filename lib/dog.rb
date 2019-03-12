4447b7bfe5ab989a8b2c31d90f4bfd1c388e5788
Ay Has <ahashim983@yahoo.com>
2019-03-12 18:05:08 +0000

    Automatically backed up by Learn

---
+++ lib/dog.rb
@@ -0,0 +1,28 @@
+class Dog
+
+  def initialize(name)
+    @name = name
+ end
+
+  def name=(dog_name)
+    @name = dog_name
+  end
+
+  def name(dog_name)
+    @name = dog_name
+  end
+
+  def initialize(breed)
+    @breed = breed
+  end
+
+
+  def breed=(dog_breed)
+    @breed = dog_breed
+  end
+
+  def breed(dog_breed)
+    @breed = dog_breed
+  end
+
+end
